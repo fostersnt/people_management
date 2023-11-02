dbd3326c85735abcba23ec7da5ff52c03359eb30
= <=>
2023-11-02 13:49:48 +0000

    Added centralized store

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,52 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  </div>
+  <div>
+    <form @submit.prevent="getAPI_data" action="">
+      <div>
+        <label for="name">Name</label>
+        <input type="text" v-model="formData.name" name="" id="">
+      </div>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+
+  data(){
+    return {
+      formData: {
+        name: '',
+      },
+      baseURL: 'https://catfact.ninja/',
+    }
+  },
+
+  methods: {
+    getAPI_data(){
+      let url = this.baseURL;
+      let endpoint = 'fact';
+      
+      axios.get(url + endpoint)
+      .then(response => {
+        this.formData.name = response.data.fact;
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      })
+    }
   }
 }
 </script>
---
+++ src/store.js
@@ -0,0 +1,25 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    //State variables go here
+    state: {
+
+    },
+    //Mutation methods go here
+    mutations: {
+
+    },
+    //Actions go here
+    actions: {
+
+    },
+    //Getters also go here
+    getters: {
+
+    }
+});
+
+export default store;
