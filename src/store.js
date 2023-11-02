92bbce0a1590520338e9997c9abdaac8d69fca3a
= <=>
2023-11-02 13:52:16 +0000

    Added two methods in the mutation object

--- src/store.js
+++ src/store.js
@@ -6,11 +6,17 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
     //State variables go here
     state: {
-
+        name: 'Foster Asante',
+        age: 21
     },
     //Mutation methods go here
     mutations: {
-
+        UPDATE_NAME(state, newName){
+            state.name = newName;
+        },
+        UPDATE_AGE(state, newAge){
+            state.age = newAge;
+        }
     },
     //Actions go here
     actions: {
