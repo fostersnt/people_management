6898073a240f3503517e4a830343d1046a9d3e57
= <=>
2023-11-03 05:24:01 +0000

    Added two views

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from "./store/index";
+import router from './components/router/index';
 
 
 const app = createApp(App);
 
+//NB: Registration of imports should be done before mounting the app
 app.use(store);
+app.use(router)
 
 app.mount('#app')
 // createApp(App).mount('#app')
